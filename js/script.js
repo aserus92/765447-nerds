00b0314730024f2b51c63718384fa803d922e187
aserus92 <saraev92@mail.ru>
2018-07-12 03:49:44 +0300

    Добавлена анимация, различные исправления и подключены минифицированные файлы

--- js/script.js
+++ js/script.js
@@ -1,45 +1,51 @@
-		var link = document.querySelector(".button-write-us");
-		var popup = document.querySelector(".modal");
-		var close = popup.querySelector(".modal-close");
+var link = document.querySelector(".button-write-us");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
 
-		var form = popup.querySelector("form");
-		var email = popup.querySelector("[name=email]");
+var form = popup.querySelector("form");
+var nameInput = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
 
-		 var isStorageSupport = true;
-		 var storage = "";
-		  
-		 try {
-		    storage = localStorage.getItem("email");
-		 } catch (err) {
-		    isStorageSupport = false;
-		 }
+var isStorageSupport = true;
+var storage = "";
+	  
+try {
+    storage = localStorage.getItem("email");
+} catch (err) {
+    isStorageSupport = false;
+}
 
-		link.addEventListener("click",function(evt) {
-			evt.preventDefault();
-			popup.classList.add("modal-show");
-		});
+link.addEventListener("click",function(evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+	nameInput.focus();
+});
 
-		close.addEventListener("click", function (evt) {
-			evt.preventDefault();
-			popup.classList.remove("modal-show");
-		});
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
 
-		form.addEventListener("submit", function (evt) {
-    		if (!email.value) {
-    			evt.preventDefault();
-    			popup.classList.add("modal-error");	
-    		} else {
-    			if (isStorageSupport) {
-    				localStorage.setItem("email", email.value);
-    			}
-    		}
-    	});
+form.addEventListener("submit", function (evt) {
+	if (!email.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal-error");
+      	popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");	
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("email", email.value);
+		}
+	}
+});
 
-		window.addEventListener("keydown", function(evt){
-			if (evt.keyCode === 27) {
-      			evt.preventDefault();
-      			if (popup.classList.contains("modal-show")) {
-        			popup.classList.remove("modal-show");
-      			}
-   			 }
-		});
+window.addEventListener("keydown", function(evt){
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popup.classList.contains("modal-show")) {
+   			popup.classList.remove("modal-show");
+   			popup.classList.remove("modal-error");
+		}
+	 }
+});
